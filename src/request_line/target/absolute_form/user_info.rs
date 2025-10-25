a94a4089fa43a742fece5c18a2129fc9f9661ebf
a-irizi <irizi.anass.00@gmail.com>
2025-10-25 13:02:57 +0100

    extract reserved/unreserved character into request_line module and add related helper methods.

--- src/request_line/target/absolute_form/user_info.rs
+++ src/request_line/target/absolute_form/user_info.rs
@@ -1,60 +1,26 @@
 //! URL authority's user info strategies.
 
-use std::fmt::Write;
+use std::{fmt::Write, ops::RangeInclusive, sync::LazyLock};
 
-use proptest::{
-  option::of,
-  prelude::{Strategy, any},
-  prop_oneof,
-  sample::select,
+use array_concat::{concat_arrays, concat_arrays_size};
+use proptest::{option::of, prelude::Strategy};
+
+use crate::request_line::{
+  SUB_DELIMS, UNRESERVED, char_diff_intervals, safe_and_percent_encoded_char, url_chars_to_string,
 };
 
-#[derive(Debug)]
-enum UrlChar {
-  Normal(char),
-  PercentEncoded(String),
-}
+static USER_INFO_UNSAFE_CHARS: LazyLock<Vec<RangeInclusive<char>>> =
+  LazyLock::new(|| char_diff_intervals(&USER_INFO_SAFE_CHARS));
 
-const USER_INFO_SAFE_CHARS: [char; 77] = [
-  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-  't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-  'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4',
-  '5', '6', '7', '8', '9', '-', '.', '_', '~', '!', '$', '&', '\'', '(', ')', '*', '+', ',', ';',
-  '=',
-];
-
-fn url_char() -> impl Strategy<Value = UrlChar> {
-  let safe_chars_strategy = select(&USER_INFO_SAFE_CHARS).prop_map(UrlChar::Normal);
-
-  let unsafe_chars_strategy =
-    any::<char>().prop_filter_map("unreserved, sub-delims or colon", |c: char| {
-      if USER_INFO_SAFE_CHARS.contains(&c) {
-        None
-      } else {
-        let mut pct_encoded = String::with_capacity(c.len_utf8() * 3);
-        for byte in c.encode_utf8(&mut [0u8; 4]).bytes() {
-          let _ = write!(pct_encoded, "%{byte:02x}");
-        }
-
-        Some(UrlChar::PercentEncoded(pct_encoded))
-      }
-    });
-
-  prop_oneof![98 => safe_chars_strategy, 2 => unsafe_chars_strategy]
-}
+const USER_INFO_SAFE_CHARS: [char; concat_arrays_size!(UNRESERVED, SUB_DELIMS)] =
+  concat_arrays!(UNRESERVED, SUB_DELIMS);
 
 fn user_info_subcomponent() -> impl Strategy<Value = String> {
-  proptest::collection::vec(url_char(), 0..=50).prop_map(|chars| {
-    let mut result = String::new();
-    for c in chars {
-      match c {
-        UrlChar::Normal(c) => result.push(c),
-        UrlChar::PercentEncoded(pct_encoded) => result.push_str(&pct_encoded),
-      }
-    }
-
-    result
-  })
+  proptest::collection::vec(
+    safe_and_percent_encoded_char(&USER_INFO_SAFE_CHARS, &*USER_INFO_UNSAFE_CHARS),
+    0..=50,
+  )
+  .prop_map(url_chars_to_string)
 }
 
 #[derive(Debug)]
