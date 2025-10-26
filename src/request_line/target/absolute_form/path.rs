29e9055bdf5a698214f54a6f96752d3affecef70
a-irizi <irizi.anass.00@gmail.com>
2025-10-26 16:36:10 +0100

    make absolute_path and rootless_path return the path along with it's normalized version

--- src/request_line/target/absolute_form/path.rs
+++ src/request_line/target/absolute_form/path.rs
@@ -1,9 +1,9 @@
 //! URL path strategies.
 
-use std::{fmt::Write, num::NonZero, ops::RangeInclusive, sync::LazyLock};
+use std::{num::NonZero, ops::RangeInclusive, sync::LazyLock};
 
 use array_concat::{concat_arrays, concat_arrays_size};
-use proptest::{option::of, prelude::Strategy};
+use proptest::prelude::Strategy;
 
 use crate::request_line::{
   SUB_DELIMS, UNRESERVED, UrlChar, char_diff_intervals, safe_and_percent_encoded_char,
@@ -28,22 +28,70 @@ fn segment_nz(max_chars: usize) -> impl Strategy<Value = String> {
   segment(1, max_chars)
 }
 
-/// rootless path with no query params and no fragment.
-pub fn path_rootless(max_segments: NonZero<usize>) -> impl Strategy<Value = String> {
-  (segment_nz(50), proptest::collection::vec(segment(0, 50), 0..=max_segments.get())).prop_map(
-    |(mut segment_nz, segments)| {
-      let segments = segments.join("/");
-      let _ = write!(segment_nz, "/{segments}");
+/// URL path.
+#[derive(Debug)]
+pub struct Path {
+  /// normalized path.
+  /// # Examples
+  /// * the path `"/foo/./bar"` will be normalized to `"/foo/bar"`
+  /// * the path `"/foo/../bar"` will be normalized to `"/bar"`
+  pub normalized: String,
+}
 
-      segment_nz
+/// rootless path with no query params and no fragment.
+/// # Returns
+/// [`Path`] and its raw representation.
+pub fn path_rootless(max_segments: NonZero<usize>) -> impl Strategy<Value = (Path, String)> {
+  (segment_nz(50), proptest::collection::vec(segment(0, 50), 0..=max_segments.get())).prop_map(
+    |(segment_nz, segments)| {
+      let repr = if segments.is_empty() {
+        segment_nz.clone()
+      } else {
+        format!("{segment_nz}/{segments}", segments = segments.join("/"))
+      };
+
+      let mut normalized_path_segments = vec![];
+
+      if segment_nz != "." && segment_nz != ".." {
+        normalized_path_segments.push(segment_nz);
+      }
+
+      let segment_count = segments.len();
+      for (idx, segment) in segments.into_iter().enumerate() {
+        match segment.as_str() {
+          "." => {
+            if idx == segment_count - 1 {
+              normalized_path_segments.push(String::new());
+            }
+          }
+          ".." => {
+            normalized_path_segments.pop();
+          }
+          _ => normalized_path_segments.push(segment),
+        }
+      }
+
+      (
+        Path {
+          normalized: if normalized_path_segments.is_empty() {
+            "/".to_string()
+          } else {
+            normalized_path_segments.join("/")
+          },
+        },
+        repr,
+      )
     },
   )
 }
 
 /// absolute path with no query params and no fragment.
-pub fn path_absolute(max_segments: NonZero<usize>) -> impl Strategy<Value = String> {
-  of(path_rootless(max_segments))
-    .prop_map(|segments| format!("/{}", segments.as_deref().unwrap_or("")))
+/// #Returns
+/// [`Path`] and its raw representation.
+pub fn path_absolute(max_segments: NonZero<usize>) -> impl Strategy<Value = (Path, String)> {
+  path_rootless(max_segments).prop_map(|(path, repr)| {
+    (Path { normalized: format!("/{}", path.normalized) }, format!("/{repr}"))
+  })
 }
 
 #[cfg(test)]
@@ -55,10 +103,10 @@ mod tests {
   use super::*;
 
   proptest! {
-    #![proptest_config(ProptestConfig::with_cases(100_000))]
+    #![proptest_config(ProptestConfig::with_cases(10_000))]
     #[test]
-    fn path_absolute_works(path in path_absolute(NonZeroUsize::new(25).unwrap())) {
-      assert!(path.starts_with('/'));
+    fn path_absolute_works((_, repr) in path_absolute(NonZeroUsize::new(25).unwrap())) {
+      assert!(repr.starts_with('/'));
     }
   }
 }
